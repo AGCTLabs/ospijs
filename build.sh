894d2068e93e1685e52c83ec31f7265e3d928223
Shiva <agct.labs@gmail.com>
2013-08-09 13:45:36 -0700

    Clean up and seutp auto build

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-npm install
-
 rm -rf lib/libospi.so lib/libospi.so.1.0.1 build
-mkdir build
+mkdir build logs
 
 gcc -fPIC -g -c -Wall src/ospijs.c  -o build/ospijs.o
 gcc -shared -Wl,-soname,libospi.so -o libospi.so.1.0.1 build/ospijs.o -lc
